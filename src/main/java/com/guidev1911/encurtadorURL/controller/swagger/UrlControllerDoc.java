f46a96b470cf8ebca70fbdef66b9bc330eac35e0
guidev1911 <guilhermereal1911@gmail.com>
2025-05-20 19:04:00 -0300

    doc swagger

---
+++ src/main/java/com/guidev1911/encurtadorURL/controller/swagger/UrlControllerDoc.java
@@ -0,0 +1,19 @@
+package com.guidev1911.encurtadorURL.controller.swagger;
+import com.guidev1911.encurtadorURL.dto.UrlRequest;
+import com.guidev1911.encurtadorURL.dto.UrlResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public interface UrlControllerDoc {
+
+    @Operation(summary = "Cria uma URL encurtada")
+    @ApiResponse(responseCode = "200", description = "URL encurtada criada com sucesso")
+    @PostMapping("/shorten")
+    public ResponseEntity<UrlResponse> shorten(@RequestBody UrlRequest request);
+
+    @Operation(summary = "Redireciona a URL encurtada para a original")
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<?> redirect(@PathVariable String shortCode);
+}
